e9ac0abf1d17098066679a87998161b970b89674
gngdb <gavingray1729@gmail.com>
2017-08-25 12:13:11 +0100

    some DICE usability changes

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ from torch.autograd import Variable
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+parser.add_argument('--data', '-d', default=os.environ.get('SCRATCH',os.getcwd()), help='place to store data')
 args = parser.parse_args()
 
 use_cuda = torch.cuda.is_available()
@@ -41,10 +42,13 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+print(args.data)
+data_save_loc = os.path.join(args.data, 'data')
+print(data_save_loc)
+trainset = torchvision.datasets.CIFAR10(root=data_save_loc, train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+testset = torchvision.datasets.CIFAR10(root=data_save_loc, train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
@@ -60,7 +64,7 @@ if args.resume:
     start_epoch = checkpoint['epoch']
 else:
     print('==> Building model..')
-    # net = VGG('VGG19')
+    net = VGG('VGG16')
     # net = ResNet18()
     # net = GoogLeNet()
     # net = DenseNet121()
@@ -68,7 +72,7 @@ else:
     # net = MobileNet()
     # net = DPN92()
     # net = ShuffleNetG2()
-    net = SENet18()
+    # net = SENet18()
 
 if use_cuda:
     net.cuda()
