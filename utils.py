aca69c734bc5921efae8bdabe3684f09dfe49eed
ejcrowley <elliotjcrowley@gmail.com>
2017-09-07 14:11:10 +0100

    Added function that returns no params

--- utils.py
+++ utils.py
@@ -11,6 +11,18 @@ import math
 import torch.nn as nn
 import torch.nn.init as init
 
+def get_no_params(net):
+
+    params = net.state_dict()
+    tot=0
+    for p in params:
+        no = params[p].view(-1).__len__()
+        tot += no
+        print('%s has %d params' % (p,no))
+    print('Net has %d params in total' % tot)
+    return tot
+
+
 
 def get_mean_and_std(dataset):
     '''Compute the mean and std value of dataset.'''
