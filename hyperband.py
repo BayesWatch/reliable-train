581404aefdfea8596af6b97f0773bc18e98bed83
gngdb <gavingray1729@gmail.com>
2017-09-15 17:25:07 +0100

    explicit infinite loop

--- hyperband.py
+++ hyperband.py
@@ -56,23 +56,24 @@ class Hyperband(object):
                 T = [T[i] for i in np.argsort(self.val_losses)[0:int(n_i/eta)]]
 
     def get_next_checkpoint(self):
-        while not all([pc[1]>0 for pc in prescriptions]):
-            # return each checkpoint until they run out of prescribed epochs
-            # then fill self.val_losses with the current losses and iterate the config generator
-            prescription = prescriptions[self.prescription_idx]
-            if self.prescription_idx < len(prescriptions):
-                self.prescription_idx += 1
-            else:
-                self.prescription_idx = 0
+        while True:
+            while not all([pc[1]>0 for pc in prescriptions]):
+                # return each checkpoint until they run out of prescribed epochs
+                # then fill self.val_losses with the current losses and iterate the config generator
+                prescription = prescriptions[self.prescription_idx]
+                if self.prescription_idx < len(prescriptions):
+                    self.prescription_idx += 1
+                else:
+                    self.prescription_idx = 0
 
-            if prescription[1] > 0:
-                prescription[1] += -1
-                return prescription[0]
+                if prescription[1] > 0:
+                    prescription[1] += -1
+                    return prescription[0]
 
-        # we must have run out of prescribed epochs, get some more
-        for i in range(len(self.val_losses)):
-            self.val_losses[i] = prescriptions[i][0].best_saved['loss']
-        prescriptions = next(self.configuration_generator)
+            # we must have run out of prescribed epochs, get some more
+            for i in range(len(self.val_losses)):
+                self.val_losses[i] = prescriptions[i][0].best_saved['loss']
+            prescriptions = next(self.configuration_generator)
 
     def get_random_hyperparameter_configution(self):
         raise NotImplementedError()
