997b73c977cf5009dbb3ac24a81dde50cb14aeb3
gngdb <gavingray1729@gmail.com>
2017-09-29 12:35:54 +0100

    to demo progress, wait

--- dummy.py
+++ dummy.py
@@ -1,10 +1,12 @@
 
 import numpy.random as npr
+import time
 
 print("some other stuff")
 
 if __name__ == '__main__':
     # return random scores and occasionally throw errors
+    time.sleep(0.1)
     if npr.rand() < 0.95:
         print(npr.rand())
     else:
