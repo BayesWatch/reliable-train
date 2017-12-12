e0e5c54a183cfefc50c9372400b5dc5b51f8421f
gngdb <gavingray1729@gmail.com>
2017-12-12 14:40:34 +0000

    exact sparsity based on Jack's code, with test

--- deep_compression.py
+++ deep_compression.py
@@ -1,3 +1,6 @@
+
+import numpy as np
+
 import torch
 from torch.optim import SGD
 
@@ -65,3 +68,49 @@ class MaskedSGD(SGD):
         if batch_index > threshold and not self.__dict__.get('sparsified', False):
             self.sparsify()
             self.sparsified = True
+
+class ExactSparsity(MaskedSGD):
+    """Modification to enforce an exact sparsity goal by setting the threshold
+    according to this goal. `sparsity_goal` must be given as a value between 0
+    and 1."""
+    def __init__(self, params, sparsity_goal, lr=0.1, momentum=0, dampening=0,
+                 weight_decay=0, nesterov=False):
+        self.goal = sparsity_goal
+        super(ExactSparsity, self).__init__(params, lr=lr, momentum=momentum,
+                dampening=dampening, weight_decay=weight_decay,
+                nesterov=nesterov)
+
+    def sparsify(self):
+        """Sets a mask that will zero all parameters below 0.02 while this
+        optimizer continues to be used."""
+        threshold = self.get_threshold()
+        for group in self.param_groups:
+            group['masks'] = []
+            for p in group['params']:
+                group['masks'].append(torch.gt(torch.abs(p),threshold).float())
+
+    def get_threshold(self):
+        # gather all parameters in one massive numpy array
+        combined_params = None
+        for group in self.param_groups:
+            group['masks'] = []
+            for p in group['params']:
+                np_p = torch.abs(p).data.cpu().numpy().ravel()
+                if combined_params is None:
+                    combined_params = np_p
+                else:
+                    combined_params = np.concatenate([combined_params,np_p])
+        return np.percentile(combined_params, 100-self.goal*100)
+
+if __name__ == '__main__':
+    from models import *
+    from checkpoint import sparsity
+    net = VGG('VGG16')
+    print("Sparsity at initialisation: %f"%sparsity(net))
+    print("Setting goal to 0.2")
+    optimizer = ExactSparsity(net.parameters(),0.2)
+    x = Variable(torch.randn(2,3,32,32))
+    net(x).sum().backward()
+    optimizer.sparsify()
+    optimizer.step()
+    print("Sparsity after sparsification: %f"%sparsity(net))
